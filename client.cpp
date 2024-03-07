6e64ac061d61b75c73eceb78a392d8abdb32d636
Belijj <145497420+Belijj@users.noreply.github.com>
2024-03-07 11:58:10 +0000

    .

--- client.cpp
+++ client.cpp
@@ -2,61 +2,48 @@
 #include <cstring>
 #include <unistd.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 int PORT = 8888;
+using namespace std;
 
-int main(int argc, char **argv) {
-    if(argc > 1) {
+int main(int argc, char *argv[]) {
+    
+    if (argc > 1) {
         PORT = atoi(argv[1]);
     }
 
-    int server_fd, new_socket;
+    int sock, new_socket;
     long valread;
-    struct sockaddr_in address;
-    int addrlen = sizeof(sockaddr_in);
-    const char *hello = "Hello from server";
+    struct sockaddr_in serv_address;
+    int addrlen = sizeof(serv_address);
+    char *hello = "Hello from server";
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    char buffer[1024] = {0};
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket Failed");
         exit(EXIT_FAILURE);
     }
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    serv_address.sin_family = AF_INET;
+    serv_address.sin_port = htons(PORT);
 
-    memset(address.sin_zero, '\0', sizeof(address.sin_zero));
-
-    if(bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_address.sin_addr) <= 0) {
+        cout << "Invalid address" << endl;
+        return -1;
     }
 
-    if(listen(server_fd, 10) < 0) {
-        perror("Listen");
-        exit(EXIT_FAILURE);
+    if (connect(sock, (struct sockaddr *)&serv_address, sizeof(serv_address)) < 0) {
+        cout << "Connection Failed" << endl;
+        return -1;
     }
 
-    std::cout << "Listening on port " << PORT << "..." << std::endl;
-
-    while(true) {
-        std::cout << "Waiting for new connection ..." << std::endl;
-        if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("accept");
-            continue;
-        }
-        char buffer[30000] = {0};
-        valread = read(new_socket, buffer, 30000);
-        std::cout << "Number of bytes received: " << valread << std::endl;
-        if(valread > 0) {
-            std::cout << "Received message: " << buffer << std::endl;
-        }
-        write(new_socket, hello, strlen(hello));
-        std::cout << "Hello message sent" << std::endl;
-        if(buffer[0] == 'e') {
-            close(new_socket);
-        }
-    }
+    send(sock, hello, strlen(hello), 0);
+    cout << "Hello message sent" << endl;
+    valread = read(sock, buffer, 1024);
+    cout << "Message Received: " << buffer << endl;
 
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,62 +1,69 @@
 #include <iostream>
+#include <netinet/in.h>
 #include <cstring>
 #include <unistd.h>
-#include <netinet/in.h>
 
-int PORT = 8888;
+#define SOCKET_OPENING_ERROR -1
+#define BIND_ERROR -2
+#define LISTEN_ERROR -2
+#define ZBS 0
 
-int main(int argc, char **argv) {
-    if(argc > 1) {
-        PORT = atoi(argv[1]);
-    }
+using namespace std;
+const int PORT = 8888;
 
-    int server_fd, new_socket;
+int main()
+{
+    int server_fd;
+    int new_socket;
     long valread;
-    struct sockaddr_in address;
-    int addrlen = sizeof(sockaddr_in);
-    const char *hello = "Hello from server";
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket Failed");
-        exit(EXIT_FAILURE);
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    
+    char *hello = "Vi kto takie? ja vas suda ne zval!\n";
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(server_fd == 0){
+        cout << "Socket failed" << endl;
+        exit(SOCKET_OPENING_ERROR);
     }
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    memset(address.sin_zero, '\0', sizeof(address.sin_zero));
+    cout << PORT << endl;
+    cout << htons(PORT) << endl;
 
-    if(bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+    int bind_result = bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    if(bind_result < 0){
+        cout  << "BIND Failed" << endl;
+        exit(BIND_ERROR);
     }
 
-    if(listen(server_fd, 10) < 0) {
-        perror("Listen");
-        exit(EXIT_FAILURE);
+    int listen_result = listen(server_fd, 10);
+    if(listen_result < 0){
+        cout  << "Listen Failed" << endl;
+        exit(LISTEN_ERROR);
     }
-
-    std::cout << "Listening on port " << PORT << "..." << std::endl;
-
-    while(true) {
-        std::cout << "Waiting for new connection ..." << std::endl;
-        if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("accept");
-            continue;
-        }
-        char buffer[30000] = {0};
-        valread = read(new_socket, buffer, 30000);
-        std::cout << "Number of bytes received: " << valread << std::endl;
-        if(valread > 0) {
-            std::cout << "Received message: " << buffer << std::endl;
-        }
-        write(new_socket, hello, strlen(hello));
-        std::cout << "Hello message sent" << std::endl;
-        if(buffer[0] == 'e') {
+    cout << "Listening on port: " << PORT << endl;
+    
+        while(true){
+            cout << "Waiting for new connection ..." << endl;
+            new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+            if(new_socket < 0){
+                continue;
+            }
+            char buffer[30000] = {0};
+            valread = read(new_socket, buffer, 30000);
+            cout << "Number of bytes received: " << valread << endl;
+            if(valread > 0){
+                cout << "Message received: " << buffer << endl;
+            }
+            write(new_socket, hello, strlen(hello));
+            cout << "Hello message sent" << endl;
             close(new_socket);
         }
-    }
-
-    return 0;
+    exit(ZBS);
 }
